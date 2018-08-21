e5bfb6543855ad7b2cdaeefa2bbaa445e38f1226
guysinger00 <guysinger00@gmail.com>
2018-08-21 22:12:12 +0300

    some change

--- java/input_output/input_output_example.java
+++ java/input_output/input_output_example.java
@@ -5,7 +5,7 @@ public class input_output_example {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int var = 5;
+		int var = 6;
 		System.out.println(var);
 		
 	}
