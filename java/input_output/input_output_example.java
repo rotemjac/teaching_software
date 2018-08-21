31c27cd850f4c619daf355490409bf7ed4fa5b3d
guysinger00 <guysinger00@gmail.com>
2018-08-21 22:08:51 +0300

    adding input output topic

---
+++ java/input_output/input_output_example.java
@@ -0,0 +1,13 @@
+package input_output;
+
+public class input_output_example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int var = 5;
+		System.out.println(var);
+		
+	}
+
+}
